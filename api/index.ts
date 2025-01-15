f657301fb917ef70bb0b96eb5162e75f3c375288
melissaa55 <trenggonomeli@gmail.com>
2025-01-16 03:38:09 +0700

    fix: revert vercel prep

--- api/index.ts
+++
@@ -1,6 +0,0 @@
-import { handle } from '@hono/node-server/vercel';
-
-// @ts-expect-error - ignore
-import app from '../.dist/src/index';
-
-export default handle(app);
---
+++ src/configs/env.config.example.ts
@@ -0,0 +1,25 @@
+import { z } from 'zod';
+
+const EnvSchema = z.object({
+  PORT: z.coerce.number().default(8787),
+  DATABASE_URL: z
+    .string()
+    .url()
+    .default('postgresql://username:password@host/db'),
+  JWT_SECRET: z.string().default('THIS-IS-YOUR-SECRET-KEY'),
+  TOKEN_EXPIRATION_TIME: z.coerce.number().default(3600),
+  ALLOWED_ORIGINS: z
+    .string()
+    .default('["*"]')
+    .transform((value) => JSON.parse(value.replace(/\\/g, '')))
+    .pipe(z.array(z.string().url())),
+});
+
+const result = EnvSchema.safeParse(process.env);
+if (!result.success) {
+  console.error('Invalid environment variables: ');
+  console.error(result.error.flatten().fieldErrors);
+  process.exit(1);
+}
+
+export const env = result.data;
